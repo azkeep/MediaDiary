459d1dccbffae966bc784a6f91ad91fb767d5773
Oleksandr-Braiko <azkeep@gmail.com>
2025-12-13 19:48:47 +0200

    Initial commit

---
+++ src/main/java/azkeep/mediadiary/MediaDiaryApplication.java
@@ -0,0 +1,13 @@
+package azkeep.mediadiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/azkeep/mediadiary/MediaDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package azkeep.mediadiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
